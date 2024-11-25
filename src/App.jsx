0a54a0d56447412f9b2525b88db96b9542a54d8c
Aidan Bigornia <aidanmbigornia@gmail.com>
2024-11-24 19:38:52 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import TodoList from './components/TodoList'
+import { DebounceInput } from 'react-debounce-input'
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+  const [inputValue, setInputValue] = useState('');
+  
+  const handleAddTodo = () => {
+    if (inputValue.trim()) {
+      setTodos([
+        ...todos,
+        {
+          id: uuidv4(),
+          name: inputValue,
+          done: false,
+        },
+      ]);
+      setInputValue('');
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleAddTodo();
+    }
+  };
+
+  const handleCompleteTodo = (id) => {
+    const updatedTodoList = todos.map(todo => {
+        if (todo.id === id) {
+            return  {
+              ...todo,
+              done: !todo.done,
+            }
+        }
+        return todo;
+    });
+    setTodos(updatedTodoList);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const index = todos.findIndex(todo => todo.id === id);
+    const updateTodo = [...todos];
+    if (index !== -1) {
+      updateTodo.splice(index, 1);
+    }
+    setTodos(updateTodo);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="app-wrapper">
+      <div className="addtodo-wrapper">
+        <DebounceInput
+          className="newtodo-input"
+          spellCheck='false'
+          debounceTimeout={300}
+          placeholder='Create new task'
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}  
+        />
+        <button className="newtodo-btn" onClick={handleAddTodo}>Add Task</button>
+      </div>
+      <TodoList todos={todos} onComplete={handleCompleteTodo} onDelete={handleDeleteTodo}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,28 @@
+import './TodoList.css'
+
+const TodoList = ({ todos, onComplete, onDelete }) => {
+    return (
+        todos.map((todo, index) => (
+            <div className={`todo-item ${todo.done && 'todo-completed'}`} key={index}>
+                <div className="todo-name" >{todo.name}</div>
+                <div className="todo-options">
+                    <div className="complete" onClick={() => onComplete(todo.id)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#000000">
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M9 16.2l-3.5-3.6L4 14l5 5 10-10-1.4-1.4z"/>
+                        </svg>
+                    </div>
+
+                    <div className="remove" onClick={() => onDelete(todo.id)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#000000">
+                            <path d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M16 9v10H8V9h8m-1.5-6H9.5l-1 1H5v2h14V4h-3.5l-1-1z"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        ))
+    )
+}
+
+export default TodoList;
